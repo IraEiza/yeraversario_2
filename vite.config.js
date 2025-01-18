9729d9363b0a93c67a8a76ff179930dcbeb9b070
IraEiza <iratze.eizaguirre@gmail.com>
2025-01-18 23:55:26 +0000

    changed vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,8 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/yeraversario_2/'
+  base: '/yeraversario_2/', // Reemplaza REPO_NAME con el nombre de tu repositorio
+  build: {
+    outDir: 'docs' // Genera los archivos en la carpeta 'docs'
+  }
 })
